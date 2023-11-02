5a8db5d18759b18a5c62e7c4f3f1ef27339bbca6
BernadusRaka <bernadusraka@gmail.com>
2023-11-02 23:55:30 +0700

    Mini Project

---
+++ Mobile-Testing/Mobile-Test-AltaShop/Test Suites/Home Page.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Home Page</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>70a598d4-103a-4201-9e0f-d61e2ad471e0</testSuiteGuid>
+</TestSuiteEntity>
